20d0ec0c679170c40e010f2c5d042421925836a2
Michaelangelo <ramos.michael85@gmail.com>
2019-10-18 03:17:47 +0000

    Fix image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_132242) do
+ActiveRecord::Schema.define(version: 2019_10_17_225336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_132242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
@@ -36,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_10_15_132242) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
